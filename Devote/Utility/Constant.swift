a6e46219a04f4fcd1ff5ed3e9e75dd0a5c9104ce
AlexBQ7 <alejandro.barreto.quintana@gmail.com>
2022-12-13 13:00:11 -0600

    Setup Devote App

---
+++ Devote/Utility/Constant.swift
@@ -0,0 +1,20 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Alex Barreto on 13/12/22.
+//
+
+import Foundation
+
+// MARK: - FORMATTER
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
+
+// MARK: - UX
